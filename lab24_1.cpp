b5947e4f9936548af9f92e02bfbaa1fc049d0db3
lab6 <chaidan254905@gmail.com>
2026-03-10 18:05:15 +0700

    lab24_1.cpp

--- lab24_1.cpp
+++ lab24_1.cpp
@@ -64,3 +64,55 @@ void List::append(int d){
 }
 
 //Write List::remove() here
+void List::remove(int idx){
+    if(root == NULL) return;
+
+    Node *toDelete;
+
+    if(idx == 0){
+        toDelete = root;
+        root = root->next;
+        delete toDelete;
+        return;
+    }
+
+    Node *current = root;
+    for(int i = 0; i < idx-1; i++){
+        if(current->next == NULL) return;
+        current = current->next;
+    }
+
+    toDelete = current->next;
+    if(toDelete == NULL) return;
+
+    current->next = toDelete->next;
+    delete toDelete;
+}
+
+int main() {
+	
+List myList = {0,0};
+myList.append(5);
+myList.append(7);
+myList.append(11);
+myList.append(4);
+myList.append(12);
+myList.append(45);
+
+myList.insert(29,2);
+myList.insert(33,3);
+myList.insert(77,5);
+myList.insert(69,0);
+myList.show();
+cout << "\n"; 
+
+myList.remove(2);
+myList.show();
+cout << "\n"; 
+myList.remove(4);
+myList.show();
+cout << "\n";
+myList.remove(0);
+myList.show();
+cout << "\n";
+}
